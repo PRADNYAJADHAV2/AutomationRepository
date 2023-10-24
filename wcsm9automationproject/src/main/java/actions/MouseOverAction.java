bab23d31a913367c9d3ab9258e0cd618f322f599
vikra <vikra@DESKTOP-616JPAE>
2023-10-24 20:50:04 +0530

    Action Commit

---
+++ wcsm9automationproject/src/main/java/actions/MouseOverAction.java
@@ -0,0 +1,30 @@
+package actions;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class MouseOverAction {
+	public static void main(String[] args) throws InterruptedException {
+WebDriver	driver	=new ChromeDriver();
+driver.manage().window().maximize();
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+driver.get("https://www.bluestone.com/");
+Thread.sleep(2000);
+//handle popup
+driver.findElement(By.id("denyBtn")).click();
+
+//perform mouseHover on watch jwallery
+WebElement watchjwellry = driver.findElement(By.xpath("//a[@title='Watch Jewellery']"));
+Actions act = new Actions(driver);
+act.moveToElement(watchjwellry).perform();
+driver.findElement(By.xpath("//a[text()='Band']")).click();
+Thread.sleep(2000);
+driver.quit();
+	}
+
+}
